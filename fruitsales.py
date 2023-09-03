82e2805fe81f0c92562462bb012f344be35393b7
Ricky Nelakurti <rickyreadyn@gmail.com>
2023-09-02 20:27:53 -0400

    fruitsales_final

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,19 @@
 # add your code here
+
+import pandas as pd
+
+def create_fruit_sales_csv():
+    sales_data = {
+        'Apples': [35, 41],
+        'Bananas': [21, 34]
+    }
+    
+    years = ['2017 Sales', '2018 Sales']
+
+    df = pd.DataFrame(sales_data, index=years)
+
+    df.to_csv('fruit.csv')
+    print("Data saved to fruit.csv!")
+
+if __name__ == "__main__":
+    create_fruit_sales_csv()
